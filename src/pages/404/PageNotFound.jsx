4043ce3ec60e484dce72ef4611ebb52a15807950
Adarsh kushwha <adarshkushwaha692@gmail.com>
2023-06-26 11:48:21 +0530

    pageNot found code done

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import './style.scss'
-function PageNotFound() {
-  return (
-    <div>
-      <h1>404</h1>
-    </div>
-  )
-}
+import React from "react";
 
-export default PageNotFound
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
